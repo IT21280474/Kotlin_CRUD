73c521c34ef3c98d49d0c03104bd6389eaa6f9be
Malith Madushan Liyanage <99232266+IT21280474@users.noreply.github.com>
2023-05-06 01:04:19 +0530

    insert Done

---
+++ app/src/main/java/com/example/app/DesignModel.kt
@@ -0,0 +1,8 @@
+package com.example.app
+
+data class DesignModel (
+    var designId: String? = null,
+    var designName:String? = null,
+    var designDescriptor:  String? = null,
+    var designPrice: String? = null
+)
